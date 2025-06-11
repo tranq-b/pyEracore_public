83b0ddc0d8de2f1dcf70a525699089940dcc79ab
Tranq <topiko9989@gmail.com>
2025-06-11 12:29:31 +0300

    Update license check to improve user notification
    Revised the message prompting users to send their User ID to IT, improving clarity. Commented out the alert dialog in favor of a straightforward print statement. This simplifies user communication when access is denied.

--- pyEracorePublic.tab/License.panel/check_lic.pushbutton/script.py
+++ pyEracorePublic.tab/License.panel/check_lic.pushbutton/script.py
@@ -22,13 +22,14 @@ app = __revit__.Application
 username = app.LoginUserId
 check = LicenseChecker.CheckLicenseGranted(app)
 if not check:
-    print(app.LoginUserId)
+    print("Send the your User Id to IT Depratment:\n"
+                      "Your UserID: {}".format(app.LoginUserId))
 
 
 
-    res = forms.alert("Send the your User Id to IT Depratment:\n"
-                      "Your UserID: {}".format(app.LoginUserId),
-                      ok=True, yes=False, no=False, warn_icon=False)
+    # res = forms.alert("Send the your User Id to IT Depratment:\n"
+    #                   "Your UserID: {}".format(app.LoginUserId),
+    #                   ok=True, yes=False, no=False, warn_icon=False)
 else:
     res = forms.alert("Access Granted",
                       ok=True, yes=False, no=False, warn_icon=False)
