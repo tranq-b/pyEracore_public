9e8c6a04341cd4cf4278b1585e51f6a884cda5da
Tranq <topiko9989@gmail.com>
2025-06-11 12:05:13 +0300

    Add alert for access granted in license check script
    Previously, the script only prompted users to send their User ID to IT. This update adds an "Access Granted" alert when appropriate, enhancing clarity for end users.

--- pyEracorePublic.tab/Creation.panel/check_lic.pushbutton/script.py
+++ pyEracorePublic.tab/Creation.panel/check_lic.pushbutton/script.py
@@ -29,4 +29,7 @@ if not check:
     res = forms.alert("Send the your User Id to IT Depratment:\n"
                       "Your UserID: {}".format(app.LoginUserId),
                       ok=True, yes=False, no=False, warn_icon=False)
+else:
+    res = forms.alert("Access Granted",
+                      ok=True, yes=False, no=False, warn_icon=False)
 
